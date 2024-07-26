7c1f1c351da0d3759ddd8617ee10fb030f308125
resqiar <resqiagengr@gmail.com>
2024-07-26 12:53:57 +0700

    initial commit

---
+++ src/data/data.ts
@@ -0,0 +1,111 @@
+import {
+    astro,
+    fastify,
+    flask,
+    golang,
+    htmx,
+    jdoodle,
+    nestjs,
+    passport,
+    postgres,
+    python,
+    svelte,
+    tailwind,
+    typeorm,
+    typescript
+} from './stack';
+
+export const showcaseData = [
+    {
+        title: 'Resqiar.com',
+        description:
+            "@resqiar's personal page where it becomes the place He want to share all the knowledges, thoughts, playground and everything in between.",
+        images: [
+            '/media/resqiar-1.webp',
+            '/media/resqiar-2.webp',
+            '/media/resqiar-3.webp',
+            '/media/resqiar-4.webp',
+            '/media/resqiar-5.webp',
+            '/media/resqiar-6.webp'
+        ],
+        demoURL: 'https://resqiar.hubku.com',
+        sourceURL: 'https://github.com/resqiar/resdev',
+        techs: [svelte, typescript, tailwind, golang, postgres]
+    },
+    {
+        title: 'Roof Tile Collection',
+        description: 'A collections of Roof Tile for SME Business',
+        images: ['/media/rooftile-1.webp', '/media/rooftile-2.webp'],
+        demoURL: 'https://tokoriskiageng.com',
+        techs: [astro, tailwind]
+    },
+    {
+        title: 'Algo Visualizer',
+        description: 'Visualize infamous many algorithms for showcase and learning purpose.',
+        images: ['/media/algo.webp', '/media/algo-1.webp', '/media/algo-2.webp', '/media/algo-3.webp'],
+        demoURL: 'https://resqiar.github.io/algo-visualization',
+        sourceURL: 'https://github.com/resqiar/algo-visualization',
+        techs: [svelte, typescript]
+    },
+    {
+        title: 'Binder (New)',
+        description:
+            'My personal binder extensions. Used to store additional data to organize my binder books. With Image, Code, Playground, and QR Code functionalities, effortlessly store and organize additional data within your binder books.',
+        images: [
+            '/media/binder.webp',
+            '/media/binder-1.webp',
+            '/media/binder-2.webp',
+            '/media/binder-3.webp'
+        ],
+        sourceURL: 'https://github.com/resqiar/binder',
+        demoURL: 'https://binder.hubku.com',
+        techs: [golang, htmx, tailwind, postgres]
+    },
+    {
+        title: 'Binder (Legacy)',
+        description:
+            'My personal binder extensions. Used to store additional data to organize my binder books. With Image, Code, Playground, and QR Code functionalities, effortlessly store and organize additional data within your binder books.',
+        images: [
+            '/media/binder.webp',
+            '/media/binder-1.webp',
+            '/media/binder-2.webp',
+            '/media/binder-3.webp'
+        ],
+        sourceURL: 'https://github.com/resqiar/binder-web',
+        techs: [svelte, typescript, tailwind, jdoodle]
+    },
+    {
+        title: 'Binder Server (Legacy)',
+        description:
+            "Binder server built with NestJS and Fastify. The server provides and maintains Extension's data to Binder App through its APIs.",
+        images: [
+            '/media/binder.webp',
+            '/media/binder-1.webp',
+            '/media/binder-2.webp',
+            '/media/binder-3.webp'
+        ],
+        sourceURL: 'https://github.com/resqiar/binder-server',
+        techs: [typescript, nestjs, fastify, postgres, typeorm, passport]
+    },
+    {
+        title: 'Go Bookstore',
+        description:
+            'Go Bookstore is a Golang and Postgres-based project that offers an online book store. The project also includes an admin dashboard for management of inventory. in collaboration with @Hilll19 and @nathanpasca',
+        images: [
+            '/media/bookstore.webp',
+            '/media/bookstore-1.webp',
+            '/media/bookstore-2.webp',
+            '/media/bookstore-3.webp'
+        ],
+        sourceURL: 'https://github.com/resqiar/bookstore',
+        techs: [golang, postgres]
+    },
+    {
+        title: 'AI Anime Recommender',
+        description:
+            'Personalized anime recommendation based on user-based similarity calculations. Use collaborative filtering method with Pearson and Cosine. Built in-collaboration with @Hilll19 and @nathanpasca.',
+        images: ['/media/ai-recommender.webp'],
+        sourceURL: 'https://github.com/resqiar/anime-recommender',
+        techs: [python, flask]
+    }
+];
---
+++ src/data/stack.ts
@@ -0,0 +1,69 @@
+export const svelte = {
+    title: 'Svelte',
+    url: '/media/techs/svelte.webp'
+};
+
+export const typescript = {
+    title: 'TypeScript',
+    url: '/media/techs/ts.webp'
+};
+
+export const astro = {
+    title: 'Astro',
+    url: '/media/techs/astro.webp'
+};
+
+export const tailwind = {
+    title: 'Tailwind CSS',
+    url: '/media/techs/tailwind.webp'
+};
+
+export const jdoodle = {
+    title: 'Jdoodle API',
+    url: '/media/techs/jdoodle.webp'
+};
+
+export const nestjs = {
+    title: 'NestJS',
+    url: '/media/techs/nestjs.webp'
+};
+
+export const fastify = {
+    title: 'Fastify',
+    url: '/media/techs/fastify.webp'
+};
+
+export const postgres = {
+    title: 'PostgreSQL',
+    url: '/media/techs/postgres.webp'
+};
+
+export const typeorm = {
+    title: 'TypeORM',
+    url: '/media/techs/typeorm.webp'
+};
+
+export const passport = {
+    title: 'PassportJS',
+    url: '/media/techs/passport.webp'
+};
+
+export const python = {
+    title: 'Python',
+    url: '/media/techs/python.webp'
+};
+
+export const flask = {
+    title: 'Flask',
+    url: '/media/techs/flask.webp'
+};
+
+export const golang = {
+    title: 'Golang',
+    url: '/media/techs/golang.webp'
+};
+
+export const htmx = {
+    title: 'HTMX',
+    url: '/media/techs/htmx.webp'
+};
